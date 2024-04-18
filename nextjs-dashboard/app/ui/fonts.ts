52db9e06731b7b17725cac80ee7108645a110454
Boltzline <auguste542lavi@gmail.com>
2024-04-18 21:34:48 +0100

    lusitana update

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,6 +1,6 @@
 import {Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
-export const lustiana = Lusitana({
+export const lusitana = Lusitana({
     subsets: ['latin'], 
     weight: ['400', '700']});
